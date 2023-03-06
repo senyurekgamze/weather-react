49d7067f8bf0e87c00e68fc0da0669420368396f
senyurekgamze <121289344+senyurekgamze@users.noreply.github.com>
2023-03-06 14:28:29 +0300

    Added form section

--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,41 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  let [city, setCity] = useState("");
-  let [weatherData, setWeatherData] = useState(null);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    setWeatherData(null);
-    setCity(event.target.elements.city.value);
-  }
-
-  function showWeatherData(response) {
-    console.log(response.data);
-    setWeatherData(response.data);
-  }
-
-  let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
-  if (city && !weatherData) {
-    axios.get(apiUrl).then(showWeatherData);
-  }
-
   return (
     <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <input name="city" type="search" placeholder="Type a city" />
-        <input type="submit" value="Search" />
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
       </form>
 
-      {weatherData && (
-        <ul>
-          <li>Temperature: {Math.round(weatherData.main.temp)}°C</li>
-          <li>Description: {weatherData.weather[0].description} </li>
-          <li>Humidity: {weatherData.main.humidity}%</li>
-          <li>Wind: {Math.round(weatherData.wind.speed)}km/h</li>
-          <li>
-            <img
-              alt=""
-              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-            />
-          </li>
-        </ul>
-      )}
+      <h1>Amsterdam</h1>
+      <ul>
+        <li>Monday</li>
+        <li>Mostly Cloudly</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="" /> 6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation :15%</li>
+            <li>Humidity :75%</li>
+            <li>Wind :13km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
