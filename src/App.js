d349f5507a9e0198f0cbe8193261f67706857c87
senyurekgamze <121289344+senyurekgamze@users.noreply.github.com>
2023-03-06 13:13:48 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import "./App.css";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
         <h1>Weather App</h1>
         <Weather />
-      </header>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="www.linkedin.com/in/gamze-senyurek-087094147"
+            target="_blank"
+          >
+            Gamze Senyurek
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/senyurekgamze/weather-react"
+            target="_blank"
+          >
+            open sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
