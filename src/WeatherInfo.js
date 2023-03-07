5834a117269b3c4d59b5536e46f6f8249a8dde64
senyurekgamze <121289344+senyurekgamze@users.noreply.github.com>
2023-03-07 16:15:51 +0300

    added new component and style it

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,7 +15,10 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img src={props.data.iconUrl} alt={props.data.description} />
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
             <div className="float-left">
               <span className="temperature">
                 {" "}
@@ -26,8 +30,8 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity :{props.data.humidity}</li>
-            <li>Wind :{props.data.wind}</li>
+            <li>Humidity :{props.data.humidity} %</li>
+            <li>Wind :{props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
